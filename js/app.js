89f2e7c24ab369cd28b93e9c901b8923628f4c57
vargasjuanj <juanjovargas443377@gmail.com>
2021-07-05 23:40:30 -0300

    Media queries

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 const menu = document.getElementById('menu');
 window.onscroll = function () {
-    if (window.pageYOffset > 10) {
+    if (window.pageYOffset > 10) { // posicion vertical
         menu.style.backgroundColor = '#fff';
     }
     else {
